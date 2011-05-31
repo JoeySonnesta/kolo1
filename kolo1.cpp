598282c0a3076a638fe4f374064bb08031e94f2f
tunk <tunk@tunk>
2011-05-31 12:08:15 +0200

    komentarz o autorze

--- kolo1.cpp
+++ kolo1.cpp
@@ -1,17 +1,9 @@
+// Imie, Nazwisko, grupa(specjalnosc), nr. indeksu
 
 #include <iostream>
-
 using namespace std;
 
-void pytanie() {
-  cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;
-  cout<<"A1) ..."<<endl;
-  cout<<"Q2) Co sie zmienilo?"<<endl;
-  cout<<"A2) ..."<<endl;
-}
-
 int main(int argc, char **argv) {
-  pytanie();
   return 0;
 }
 
--- kolo1.java
+++ kolo1.java
@@ -1,15 +1,7 @@
-
+// Imie, Nazwisko, grupa(specjalnosc), nr. indeksu
 
 class Kolo1 {
-  static void pytanie() {
-    System.out.println("Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?");
-    System.out.println("A1) ...");
-    System.out.println("Q2) Co sie zmienilo?");
-    System.out.println("A2) ...");
-  }
-
   public static void main(String[] args) {
-    pytanie();
   }
 
 }

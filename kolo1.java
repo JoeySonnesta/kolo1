7c034a70a915f22883ce29be46c683fadc4b1999
tunk <tunk@tunk>
2011-05-31 12:03:51 +0200

    funkcja z pytaniem

--- kolo1.java
+++ kolo1.java
@@ -1,6 +1,16 @@
 
+
 class Kolo1 {
-  public static void main(String[] args) {
+  static void pytanie() {
+    System.out.println("Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?");
+    System.out.println("A1) ...");
+    System.out.println("Q2) Co sie zmienilo?");
+    System.out.println("A2) ...");
   }
+
+  public static void main(String[] args) {
+    pytanie();
+  }
+
 }
 
